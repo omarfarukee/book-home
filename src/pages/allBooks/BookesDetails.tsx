c7ee603c4f244dcc41d2c521f247856e191c7b31
omarfarukee <umarfaruque1920@gmail.com>
2023-07-23 16:22:46 +0600

    icon change

--- src/pages/allBooks/BookesDetails.tsx
+++ src/pages/allBooks/BookesDetails.tsx
@@ -112,6 +112,7 @@ export default function BooksDetails() {
         </div>
         <div className="mb-4">
           <h3 className="text-2xl font-bold mb-2">Reviews</h3>
+
           <div>
             {revData?.map((review: any) => (
               <div>
