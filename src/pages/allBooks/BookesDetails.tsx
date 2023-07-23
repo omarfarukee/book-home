789df717aee61295f0f2456e3c8aa1005c069c02
omarfarukee <umarfaruque1920@gmail.com>
2023-07-23 08:03:51 +0600

    home route check done

--- src/pages/allBooks/BookesDetails.tsx
+++ src/pages/allBooks/BookesDetails.tsx
@@ -148,7 +148,7 @@ export default function BooksDetails() {
             onClick={handleReviewSubmit}
             disabled={!review}
           >
-            Submit Review
+            Submit Review.
           </button>
         </div>
       </div>
